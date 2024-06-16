75ce87a21aa9777ca5696656425759a2e42ec43f
lsvleticiavieira <lcanarinho@hotmail.com>
2024-06-16 17:45:56 -0300

    API para consulta CEP. Primeira versão.

---
+++ apicep.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+
+while True:
+    try:
+        cod_cep = input("Força o código CEP: ")
+        if len(cod_cep) !=8:
+            print("Atenção, seu código CEP precisa ter 08 caracteries. Forneça outro código: ")
+        else:
+            api = f'https://viacep.com.br/ws/{cod_cep}/json/'
+            consulta = requests.get(api)
+            consulta = consulta.json()
+            consulta = pd.DataFrame(consulta)
+            print(consulta)
+
+    except ValueError:
+        print("Isso não é um código de CEP válido. Tente novamente.")
+
+
+
